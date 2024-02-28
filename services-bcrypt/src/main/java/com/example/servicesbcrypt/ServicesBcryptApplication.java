7c0bc9791a8e319edc011ade6dd38ac29498d350
DMarjar <20213tn119@utez.edu.mx>
2024-02-27 23:23:21 -0600

    [ADD] Project structure

---
+++ services-bcrypt/src/main/java/com/example/servicesbcrypt/ServicesBcryptApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicesbcrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesBcryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesBcryptApplication.class, args);
+	}
+
+}
