cfac692dd6597b42f5f7cda5bccad8f65d03fa43
Kellguzdi <20213tn114@utez.edu.mx>
2024-02-29 00:29:44 -0600

    enteoria

--- front-bcrypt/src/main.js
+++ front-bcrypt/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import { config } from 'dotenv';
 
 import './assets/main.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
--- front-bcrypt/src/views/Login.vue
+++ front-bcrypt/src/views/Login.vue
@@ -22,8 +22,10 @@
   </b-container>
 </template>
 <script>
-import CryptoJS from 'crypto-js'
+import forge from 'node-forge'
 import Swal from 'sweetalert2'
+import axios from "axios";
+
 export default {
 
   data() {
@@ -37,91 +39,76 @@ export default {
     };
   },
   methods: {
-    encryptData(data) {
-      const secretKey = import.meta.env.VUE_APP_SECRET_KEY;
-      const key = CryptoJS.enc.Utf8.parse(secretKey);
-      const iv = CryptoJS.lib.WordArray.random(128 / 8);
-      const encrypted = CryptoJS.AES.encrypt(data, key, {
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-      });
-      const encryptedDataWithIv = iv.toString(CryptoJS.enc.Hex) + encrypted.ciphertext.toString(CryptoJS.enc.Hex);
-      return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Hex.parse(encryptedDataWithIv));
-    },
-    decrypt(encryptedDataWithIv, secretKey) {
-      var bytes = CryptoJS.enc.Base64.parse(encryptedDataWithIv);
 
-      // Extraer IV (los primeros 16 bytes)
-      var iv = CryptoJS.lib.WordArray.create(bytes.words.slice(0, 4));
-      var encryptedData = CryptoJS.lib.WordArray.create(bytes.words.slice(4));
+    async submitLogin() {
 
-      var key = CryptoJS.enc.Utf8.parse(secretKey);
-      var decrypted = CryptoJS.AES.decrypt({ ciphertext: encryptedData }, key, {
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-      });
+      const email = this.credentials.email;
+      const password = this.credentials.password;
+      const jsonObject = {email,password};
+      const jsonString = JSON.stringify(jsonObject)
+      const secretKey = 'el@ConGr3z041!12'
+      console.log(secretKey)
+      if(!secretKey){
+        throw new Error('La clave secreta no existe padre')
+      }
 
-      return decrypted.toString(CryptoJS.enc.Utf8);
-    },
-    async postData(userData) {
+      // Crear un objeto cipher utilizando la clave secreta
+      const cipher = forge.cipher.createCipher('AES-CBC', secretKey);
+
+      // Generar un IV (Initialization Vector) aleatorio
+      const iv = forge.random.getBytesSync(16);
+
+      // Actualizar el objeto cipher con el IV
+      cipher.start({ iv });
+
+      // Encriptar la cadena JSON
+      cipher.update(forge.util.createBuffer(jsonString, 'utf8'));
+      cipher.finish();
+      const encryptedData = forge.util.encode64(iv+cipher.output.getBytes())
+      const dataPost = encryptedData
+      console.log(encryptedData)
       try {
-        const response = await axios.post('http://localhost:8080/api/auth', userData)
-        let usernameDecrypt = this.decrypt(response.data.username, response.data.secretKey)
-        if (usernameDecrypt) {
+        const response = await axios.post('http://localhost:8080/api/auth', dataPost);
+
+        // Obtener el IV (Initialization Vector) y los datos encriptados de la respuesta
+        const iv = forge.util.decode64(response.data.iv);
+        const encryptedData = forge.util.decode64(response.data.encryptedData);
+
+        // Crear un objeto decipher utilizando la clave secreta y el IV
+        const decipher = forge.cipher.createDecipher('AES-CBC', secretKey);
+        decipher.start({iv});
+
+        // Actualizar el objeto decipher con los datos encriptados
+        decipher.update(forge.util.createBuffer(encryptedData));
+
+        // Finalizar el proceso de descifrado
+        decipher.finish();
+        // Obtener los datos desencriptados como una cadena
+        const decryptedDataString = decipher.output.toString();
+
+        // Convertir los datos desencriptados a un objeto JSON
+        const decryptedDataObject = JSON.parse(decryptedDataString);
+
+        // Manejar los datos desencriptados según sea necesario
+        console.log('Datos desencriptados:', decryptedDataObject.data);
+        if (decryptedDataObject) {
           await Swal.fire({
             title: '¡Bienvenido!',
-            text: `Has ingresado como ${usernameDecrypt}`,
+            text: `Has ingresado como ${decryptedDataObject}`,
             icon: 'success'
           });
         }
+        // Manejar la respuesta de la API
       } catch (error) {
         await Swal.fire({
-          title: 'Error',
-          text: 'No se pudo realizar el ingreso. Por favor, intenta de nuevo.',
-          icon: 'error'
-        });
+          title:'¡Error!',
+          text:"Acceso denegado",
+          icon:"error"
+        })
+        // Manejar los errores
       }
     },
-    async submitLogin() {
-      //if (this.validateFields()) {
-
-
-      const encryptedEmail = this.encryptData(this.credentials.email);
-      const encryptedPassword = this.encryptData(this.credentials.password);
-
-
-      console.log('Encrypted Email:', encryptedEmail);
-      console.log('Encrypted Password:', encryptedPassword);
-      const userData = {
-        email: encryptedEmail,
-        password: encryptedPassword
-      };
-      this.postData(userData);
-      //}
-    },
-    /*
-    validateFields() {
-      const emailValid = this.isValidEmail(this.credentials.email);
-      const passwordValid = this.credentials.password.length >= 6;
-      return emailValid && passwordValid;
-    },
-    */
   }
-  /*
-    getEmailState() {
-    if (!this.credentials.email) return null;
-    return this.isValidEmail(this.credentials.email) ? true : false
-  },
-  getPasswordState() {
-    if (!this.credentials.password) return null;
-    return this.credentials.password.length >= 6 ? true : false;
-  },
-  isValidEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
-  }
-},*/
 }
 
 </script>
